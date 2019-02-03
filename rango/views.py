8e645e9f74ed61ccae5e9231aa058e241ea11a9e
Youssef Ramadan <2299608r@student.gla.ac.uk>
2019-02-03 20:11:53 +0000

    further changes in chapter 9

--- rango/views.py
+++ rango/views.py
@@ -138,10 +138,10 @@ def register(request):
 
             profile = profile_form.save(commit=False)
             profile.user = user
+            registered = True
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
                 profile.save()
-                registered = True
             else:
                 print(user_form.errors, profile_form.errors)
     else:
